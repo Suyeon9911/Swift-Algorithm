85e7d30d2fac059b9e8db009871178b3d0af9dab
suyeon <rlatndus9911@naver.com>
2022-10-06 05:04:51 +0900

    #40 - 프로그래머스 : 직사각형 별찍기

--- Swift-Algorithm/Swift-Algorithm/Programmers/Level1/직사각형별찍기.swift
+++ Swift-Algorithm/Swift-Algorithm/Programmers/Level1/직사각형별찍기.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+// 직사각형 별찍기
+
+func 직사각형별찍기() {
+    let n = readLine()!.components(separatedBy: [" "]).map { Int($0)! }
+    let (a, b) = (n[0], n[1])
+
+    for i in 0..<b {
+        for j in 0..<a {
+            print("*", terminator: "")
+        }
+        print("")
+    }
+
+    // 다른 풀이
+    for _ in 0..<b {
+        print(Array(repeating: "*", count: a).joined())
+    }
+}
