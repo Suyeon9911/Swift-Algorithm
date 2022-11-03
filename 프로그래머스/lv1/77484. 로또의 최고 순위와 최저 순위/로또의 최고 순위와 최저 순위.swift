5648f15488560210fcffecfea0e499195f7c0436
su_vera <rlatndus9911@naver.com>
2022-11-03 19:06:05 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.09 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.swift
@@ -0,0 +1,49 @@
+import Foundation
+
+func solution(_ lottos:[Int], _ win_nums:[Int]) -> [Int] {
+    
+    var sameNumber = 0
+    var zeroCount = 0 
+    var answer:[Int] = []
+    
+    for i in lottos {
+        if win_nums.contains(i) {
+            // 당첨번호에 포함되어 있으면 
+            sameNumber += 1
+        }
+        
+        if i == 0 {
+            zeroCount += 1
+        }
+    }
+    
+    // 0인 경우 경우의 수 따지기 
+    // 0이 1개면 -> +1 or 0, 0이 두개면 0, 1, 2  0 이 3개면 0,1,2,3 .. 
+    // 0의 개수 +1 만큼의 경우의 수 가 생김 
+    
+    answer.append(sameNumber) // 0 이 있음에도 아무것도 못맞춘 경우 
+    
+    for i in 0..<zeroCount {
+        // 0이 있는데 1개라도 맞췄을 경우?? 
+        let i = i+1
+        answer.append(sameNumber+i)
+    }
+    
+    for i in 0..<answer.count {
+        if answer[i] == 0 || answer[i] == 1 {
+            answer[i] = 6
+        } else if answer[i] == 2 {
+            answer[i] = 5
+        } else if answer[i] == 3 {
+            answer[i] = 4
+        } else if answer[i] == 4 {
+            answer[i] = 3
+        } else if answer[i] == 5 {
+            answer[i] = 2
+        } else {
+            answer[i] = 1
+        }
+    }
+    
+    return [answer.min()!, answer.max()!]
+}
