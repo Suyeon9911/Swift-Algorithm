98c803846fc500b760b37141ab46ecc56ab4aca0
su_vera <rlatndus9911@naver.com>
2022-11-25 17:21:40 +0900

    [level 2] Title: 배달, Time: 9.09 ms, Memory: 16.7 MB -BaekjoonHub

--- 프로그래머스/lv2/12978. 배달/배달.swift
+++ 프로그래머스/lv2/12978. 배달/배달.swift
@@ -1,46 +1,24 @@
 import Foundation
 
 func solution(_ N:Int, _ road:[[Int]], _ k:Int) -> Int {
-    //마을이 하나라면 자기 자신밖에 안되므로 1을 반환
-    if N == 1 { return 1}
-    //모든 거리를 저장
-    var allDistances = Array(repeating: Array(repeating: Int.max, count: N+1), count: N+1)
-    //모든 연결을 세팅
-    setConnection(n:N,connections: road, allDistances: &allDistances)
-    //플로이드로 가장 짧은 거리를 찾음
-    floyd(n: N, allDistances: &allDistances)
-    //배달 가능한 마을을 반환
-    return findEnableDelivery(n: N, allDistances: allDistances, k: k)
-}
-
-//k보다 작거나 같은 마을을 반환함.
-func findEnableDelivery(n:Int,allDistances:[[Int]],k:Int) -> Int {
-    return allDistances[1].filter{$0 <= k}.count
-}
-
-//모든 연결을 저장
-func setConnection(n:Int,connections:[[Int]],allDistances:inout[[Int]]) {
-    //자기 자신은 0으로 처리
-    for i in 1...n {
-        allDistances[i][i] = 0
+    var distance = [[Int]](repeating: [Int](repeating: 999999, count: N+1), count: N+1)
+    
+    for i in 1...N {
+        distance[i][i] = 0
     }
-    //만약 연결된 도로가 2개 이상이라면 더 작은 거리를 넣어줌.
-    for connection in connections {
-        allDistances[connection[0]][connection[1]] = min(allDistances[connection[0]][connection[1]],connection[2])
-        allDistances[connection[1]][connection[0]] = min(allDistances[connection[1]][connection[0]],connection[2])
+    
+    for i in road {
+        distance[i[0]][i[1]] = min(i[2], distance[i[0]][i[1]])
+        distance[i[1]][i[0]] = min(i[2], distance[i[1]][i[0]])
     }
-}
-
-//플로이드로 가장 짧은 거리를 넣어줌.
-func floyd(n:Int,allDistances:inout [[Int]]) {
-    for i in 1...n {
-        for j in 1...n {
-            for k in 1...n {
-                if allDistances[j][i] == Int.max || allDistances[i][k] == Int.max { continue }
-                if allDistances[j][i] + allDistances[i][k] < allDistances[j][k] {
-                    allDistances[j][k] = allDistances[j][i] + allDistances[i][k]
-                }
+    
+    for k in 1...N {
+        for i in 1...N {
+            for j in 1...N {
+                distance[i][j] = min(distance[i][j], distance[i][k] + distance[k][j] )
             }
         }
     }
+    
+    return distance[1].filter{ $0 <= k }.count
 }
