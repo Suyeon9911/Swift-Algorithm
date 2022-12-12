5febd79c26f79ea79aeb98909a19ced13801a239
su_vera <rlatndus9911@naver.com>
2022-12-12 16:50:49 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.00 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+func solution(_ n: Int) -> Int {
+    
+   var answer : Int = n + 1
+ 
+    while true {
+        if n.nonzeroBitCount == answer.nonzeroBitCount {
+            break;
+        }
+        answer += 1
+    }
+ 
+    return answer
+}
