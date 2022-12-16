2bc395492249616e44ccf2e17a2ec21ec12c36aa
su_vera <rlatndus9911@naver.com>
2022-12-16 19:18:50 +0900

    [level 2] Title: 기능개발, Time: 0.11 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.swift
@@ -0,0 +1,41 @@
+import Foundation
+
+func solution(_ progresses:[Int], _ speeds:[Int]) -> [Int] {
+    // 기능 개선 - 진도가 100프로일 떄 반영
+
+    // 뒤의 기능이 먼저 개발될 수도 있지만, 앞에 기능이 배포될 때 함께 배포
+    // 먼저 배포되어야하는 순서대로 작업 진도 배열 progresses
+    // 각 작업의 속도 speeds
+    // 각 배포마다 몇 개의 기능이 배포되는지 return
+
+    var answer: [Int] = []
+    var progresses = progresses
+    var speeds = speeds
+
+    while !progresses.isEmpty {
+
+        // 하루가 지날때마다 progresses에 speeds를 더해준다.
+        // progresses에서 100프로 이상인 것들을 제거해준다 -> 앞에서부터..
+
+        for i in 0..<progresses.count {
+            progresses[i] += speeds[i]
+        }
+
+        var count = 0
+
+        for i in 0..<progresses.count {
+            if progresses[i] >= 100 {
+                count += 1
+            } else {
+                break
+            }
+        }
+
+        progresses.removeFirst(count)
+        speeds.removeFirst(count)
+        answer.append(count)
+    }
+
+
+    return answer.filter { $0 != 0 }
+}
