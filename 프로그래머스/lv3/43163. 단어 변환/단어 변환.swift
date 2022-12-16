626131ecfb82efc8c1803666a0368cdf7e6d1723
su_vera <rlatndus9911@naver.com>
2022-12-16 18:13:10 +0900

    [level 3] Title: 단어 변환, Time: 0.02 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/lv3/43163. 단어 변환/단어 변환.swift
+++ 프로그래머스/lv3/43163. 단어 변환/단어 변환.swift
@@ -1,57 +1,47 @@
 import Foundation
 
-func isChange(_ word: String, _ now: String) -> Bool {
-    
-    if word.count != now.count {
-        return false
-    }
-    
-    var count: Int = 0
-    
-    let arrWord = Array(word)
-    let arrNow = Array(now)
-    
-    for i in 0..<arrWord.count {
-        if arrWord[i] != arrNow[i] {
-            count += 1
-        }
-        
-        if count > 1 {
-            return false
-        }
-    }
-    
-    return true
-}
-
 func solution(_ begin:String, _ target:String, _ words:[String]) -> Int {
+
+    // 타겟 단어를 만드는데 최단거리로 : BFS로 해야될 것 같음.
+
+    // 1. 탐색할 단어 tree 만들자.
+
+    func isOneDifferentWord(_ first: String, _ second: String) -> Bool {
+        let first = Array(first), second = Array(second)
+        var count = 0
+        for i in 0..<first.count {
+            if first[i] != second[i] {
+                count += 1
+            }
+        }
     
+        return count == 1 ? true : false
+    }
+
+
+    // bfs로 푸는데 큐에 1글자만 다른 경우를 넣기
     if !words.contains(target) {
         return 0
     }
     
-    var queue: [String] = []
-    queue.append(begin)
+    // BFS 
+    var visited: [Bool] = Array(repeating: false, count: words.count)
+    var queue: [(begin: String, count: Int)] = [] // 큐에 저장할 때 count도 같이 저장
     
-    var check = [Bool] (repeating: false, count: words.count)
-    var count: Int = 0
+    queue.append((begin, 0))
     
-    while !queue.isEmpty {      
-        let now = queue.popLast()!
-        if now == target {
-            break
+    while !queue.isEmpty {
+        let now = queue.removeFirst()
+        if now.begin == target {
+            return now.count
         }
         
-        count += 1
-        for idx in 0..<words.count {
-            if !check[idx] {
-                if isChange(words[idx], now) {
-                    queue.append(words[idx])
-                    check[idx] = true
-                }
+        for (index, word) in words.enumerated() {
+            if !visited[index] && isOneDifferentWord(now.begin, word) {
+                queue.append((word, now.count+1))
+                visited[index] = true
             }
         }
     }
-    
-    return count
+    return 0
 }
