f18ea40d38b1a6ffb82484a48213897d3937b5f8
su_vera <rlatndus9911@naver.com>
2023-01-13 00:42:17 +0900

    [level 2] Title: 짝지어 제거하기, Time: 265.60 ms, Memory: 40.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.swift
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.swift
@@ -1,36 +1,16 @@
 import Foundation
 
-func solution(_ s:String) -> Int {
-    
-    var stack: [Character] = []
-    var removedCount = 0
-    
-    for i in s {
-        
-        if stack.isEmpty {
-            stack.append(i)
-            continue
-        }
-        
-        let last = stack.last
-        
-        if last == i {
-            stack.removeLast(1)
-            removedCount += 2
-            continue
-        } else {
-            stack.append(i)
-            continue
+func solution(_ s:String) -> Int{
+    var STK = [Character]()
+    var arr = Array(s)
+
+    for i in arr.indices {
+        if STK.isEmpty { STK.append(arr[i]) }
+        else {
+            if STK.last! == arr[i] { STK.removeLast() }
+            else { STK.append(arr[i]) }
         }
     }
-    
-    var answer = 0 
-    
-    if stack.isEmpty {
-        if removedCount == s.count {
-            answer = 1
-        }
-    }
-    
-    return answer
+
+    return STK.isEmpty ? 1 : 0
 }
