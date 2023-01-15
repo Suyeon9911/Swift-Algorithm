42737701318a47587050f5ec24f400bbd1b97736
su_vera <rlatndus9911@naver.com>
2023-01-15 23:48:45 +0900

    [level 3] Title: 가장 먼 노드, Time: 86.33 ms, Memory: 29.7 MB -BaekjoonHub

--- 프로그래머스/lv3/49189. 가장 먼 노드/가장 먼 노드.swift
+++ 프로그래머스/lv3/49189. 가장 먼 노드/가장 먼 노드.swift
@@ -2,37 +2,37 @@ import Foundation
 
 func solution(_ n:Int, _ edge:[[Int]]) -> Int {
     var graph = Array(repeating: [Int](), count: n + 1)
-    
+
     // 인접리스트 만들어주기
     for i in edge {
         graph[i[0]].append(i[1])
         graph[i[1]].append(i[0])
     }
-    
+
     var maxCount = 0 // 먼 노드를 구하기 위해 최대 거리 저장
     var moveCountArr = Array(repeating: [Int](), count: 50001)
-    
+
     func bfs() {
         var queue = [(Int, Int)]() // 노드번호, 이동횟수
         var visited = Array(repeating: false, count: n + 1)
         queue.append((1,0))
-        
+
         visited[1] = true
         while !queue.isEmpty {
             let (node, moveCount) = queue.removeFirst()
             moveCountArr[moveCount].append(node)
             maxCount = max(moveCount, maxCount)
-            
+
             for i in graph[node] {
                 if !visited[i] {
                     queue.append((i, moveCount + 1))
-                    
+
                     visited[i] = true
                 }
             }
         }
     }
-    
+
     bfs()
     return moveCountArr[maxCount].count
 }
