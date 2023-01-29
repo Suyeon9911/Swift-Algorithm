a9ebb625241bedfd07d9209a212e39972f3e73e0
su_vera <rlatndus9911@naver.com>
2023-01-29 23:17:56 +0900

    [level 1] Title: 내적, Time: 0.02 ms, Memory: 16.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/70128. 내적/내적.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+func solution(_ a:[Int], _ b:[Int]) -> Int {
+    
+    var answer: Int = 0
+    
+    for i in 0..<a.count {
+        answer += a[i] * b[i]
+    }
+    return answer
+}
