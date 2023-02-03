54a429147c7777666a8b52e503f82404347dfcc0
su_vera <rlatndus9911@naver.com>
2023-02-04 07:33:35 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.08 ms, Memory: 16.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.swift
+++ 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.swift
@@ -2,32 +2,50 @@ import Foundation
 
 func solution(_ bridge_length:Int, _ weight:Int, _ truck_weights:[Int]) -> Int {
     
-    // 다리 길이에 맞춰 다리 만들어주기
-    var bridge = Array(repeating: 0, count: bridge_length)
-    var trucks = truck_weights 
-    var time = 0 
-    var bridgeWeight = 0 // 현재의 무게를 업데이트
-    
-    // 다리의 빈공간 만큼 배열 만들기 -> while문 돌릴때마다 트럭배열에서 제거
-    
-    while !bridge.isEmpty {
-        time += 1
-        // 다리를 건너고 있는 트럭에서 1초에 한번씩 맨 앞의 원소를 제거해준다 + 무게에서 빼준다.
-        bridgeWeight -= bridge.removeFirst() 
-        
-        // 다리에 진입할 트럭의 조건 계산 
-        if let t = trucks.first {
-            // 제한 하중에 만족하면
-            if t + bridgeWeight <= weight {
-                bridgeWeight += trucks.removeFirst()
-                bridge.append(t) // 다리에 추가 
+    // 일단 문제만 봐도 queue 자료형을 써야한다는 것.
+    // 각각 업데이트 해줄 변수들 생성
+
+    var waitTruck = truck_weights
+    waitTruck = waitTruck.reversed()
+    var time = 0
+
+    var passedTruck: [Int] = []
+    var passingTruck: [Int] = []
+
+    while !waitTruck.isEmpty {
+        // 1. 다리에 차가 없다면 건너기
+        if passingTruck.isEmpty {
+            passingTruck.append(waitTruck.removeLast())
+            time += bridge_length
+        } else {
+            // 2. 다리에 차가 있다면
+            // 2-1. 올라갈 수 있는 개수가 남았는지 확인
+            if bridge_length > passingTruck.count {
+                // 2-2. 올라갈 무게가 남아있는지 확인한다.
+                let nowSum = passingTruck.reduce(0,+)
+                // 올라갈 무게가 남았다면,
+                if (weight - nowSum) > waitTruck.last! {
+                    if !waitTruck.isEmpty {
+                        passingTruck.append(waitTruck.removeLast())
+                    }
+                    time += bridge_length
+                } else {
+                    // 올라갈 무게가 안남았다면
+                    time += 1
+                    if !passingTruck.isEmpty {
+                        passedTruck.append(passingTruck.removeFirst())
+                    }
+                }
             } else {
-                // 제한 하중이 안될 경우 
-                bridge.append(0) // 못들어가므로 0추가
+                // 이미 꽉차 있는 상태
+                time += 1
+                if !passingTruck.isEmpty {
+                    passedTruck.append(passingTruck.removeFirst())
+                }
             }
         }
     }
-    
-    
+
+    print(time)
     return time
 }
