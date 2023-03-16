08ce789aa7091bc76c9977a73be3d63a011db051
su_vera <rlatndus9911@naver.com>
2023-03-17 02:58:59 +0900

    [level 2] Title: 가장 큰 정사각형 찾기, Time: 165.08 ms, Memory: 63.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.swift
@@ -0,0 +1,49 @@
+import Foundation
+
+func solution(_ board:[[Int]]) -> Int {
+    
+    var board = board
+    var max: Int = 0
+
+    // 예외 처리 : 주어진 행이나 열이 1개 일 경우 정사각형 크기는 오직 1
+    // 예외 처리 2 : 아예 1이 없을 경우 0
+
+    if board.count == 1 || board[0].count == 1 {
+        if board.flatMap({$0}).max()! == 1 { return 1 }
+        else { return 0 }
+    }
+
+    // 좌표를 1,1로 시작해서 board.count 까지 할수도 있고, 0,0으로 시작해서 board.count-1로 할수도 있다.
+    // 후자의 경우에는 좌상단이 아니라, 좌하단의 최솟값을 1,1 에 더해줘야함.
+
+//    for i in 1..<board.count {
+//      for j in 1..<board[0].count {
+//        if board[i][j] == 1 {
+//          board[i][j] = min(board[i-1][j-1], board[i-1][j], board[i][j-1]) + 1
+//          max = max < board[i][j] ? board[i][j] : max
+//        }
+//      }
+//    }
+
+   for i in 0..<board.count-1 {
+       for j in 0..<board[0].count-1 {
+           if board[i][j] >= 1 && board[i+1][j+1] == 1 {
+               board[i+1][j+1] += min(board[i][j], board[i+1][j], board[i][j+1])
+               if board[i+1][j+1] > max { max = board[i+1][j+1] }
+           }
+       }
+   }
+
+    // 이건 내가 생각해본 방법인데..
+
+    // for i in 0..<board.count-1 {
+    //     for j in 0..<board[0].count-1 {
+    //         if board[i][j] >= 1 && board[i+1][j+1] == 1 {
+    //             board[i+1][j+1] += board[i][j] // 여기서 board의 i,j 값을 더해도 될까?
+    //             if board[i+1][j+1] > max { max = board[i+1][j+1] } // max 값 갱신
+    //         }
+    //     }
+    // }
+
+    return max * max
+}
