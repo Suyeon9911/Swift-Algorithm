67f6ae1b7dbb649012429609fc1e087e7bff7566
su_vera <rlatndus9911@naver.com>
2023-04-11 11:30:53 +0900

    [unrated] Title: 뒤에 있는 큰 수 찾기, Time: 128.72 ms, Memory: 184 MB -BaekjoonHub

--- 프로그래머스/unrated/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.swift
+++ 프로그래머스/unrated/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.swift
@@ -1,22 +1,27 @@
 import Foundation
 
 func solution(_ numbers:[Int]) -> [Int] {
-
-    var answer = Array(repeating: -1, count: numbers.count) 
-    var stack: [(Int, Int)] = [(0, numbers[0])]
-    var result: [Int] = []
     
-    for i in 1..<numbers.count {
-        // stack이 비면 종료
-        while !stack.isEmpty {
-            // stack의 마지막 값이 기준값보다 크면 break
-            if stack.last!.1 >= numbers[i] { break }
-            // stack의 마지막원소를 지우고 해당 인덱스에 큰 수 할당
-            answer[stack.removeLast().0] = numbers[i]
+    var answer = Array(repeating: -1, count: numbers.count)
+    var backMax = numbers.last!
+    
+    for i in stride(from: numbers.count-2, to: -1, by: -1) {
+        if numbers[i] >= backMax {
+            backMax = numbers[i]
+            continue
         }
-        // stack에 현재 기준값 저장
-        stack.append((i, numbers[i]))
+        
+        for j in i+1..<numbers.count {
+            if numbers[j] > numbers[i] {
+                answer[i] = numbers[j]
+                break    
+            }
+            
+            if numbers[i] >= numbers[j] && numbers[i] < answer[j] {
+                answer[i] = answer[j]
+                break
+            }
+        }
     }
-    
     return answer
 }
